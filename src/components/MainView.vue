882559263033388a38ae2c9f8725007639c3b798
Sean Wong <seanwong24@live.com>
2019-11-17 21:52:27 +0000

    add a file upload button

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -3,17 +3,29 @@
     <ion-header>
       <ion-toolbar>
         <ion-title>{{title}}</ion-title>
+        <ion-buttons slot="end">
+          <ion-button @click="upload()">
+            <ion-icon slot="icon-only" name="cloud-upload" size="large"></ion-icon>
+            <input
+              type="file"
+              id="file-upload-input"
+              accept=".json"
+              @change="fileUploaded()"
+              ref="fileUploadInput"
+            />
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     <ion-content fixed id="main-content">
       <ion-grid>
         <ion-row>
           <ion-col size="9">
-            <VisView/>
+            <VisView />
           </ion-col>
           <ion-col size="3">
             <ion-card>
-              <ControlView/>
+              <ControlView />
             </ion-card>
           </ion-col>
         </ion-row>
@@ -22,9 +34,17 @@
   </ion-app>
 </template>
 
+
 <script>
 import VisView from "./VisView";
 import ControlView from "./ControlView";
+import { addIcons } from "ionicons";
+import { cloudUpload } from "ionicons/icons";
+
+addIcons({
+  "md-cloud-upload": cloudUpload.md,
+  "ios-cloud-upload": cloudUpload.ios
+});
 
 export default {
   name: "MainView",
@@ -34,6 +54,36 @@ export default {
   components: {
     VisView,
     ControlView
+  },
+  computed: {
+    fileData: {
+      get: function() {
+        return this.$fileData;
+      },
+      set: function(value) {
+        this.$fileData = value;
+        // eslint-disable-next-line
+        console.log(this.fileData);
+      }
+    }
+  },
+  methods: {
+    upload() {
+      this.$refs.fileUploadInput.click();
+    },
+    fileUploaded() {
+      const file = this.$refs.fileUploadInput.files[0];
+      if (file && file.name.split(".").pop() === "json") {
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+          const fileContent = fileReader.result;
+          this.fileData = JSON.parse(fileContent);
+        };
+        fileReader.readAsText(file);
+      } else {
+        this.fileData = null;
+      }
+    }
   }
 };
 </script>
@@ -57,5 +107,9 @@ ion-row {
 ion-card {
   height: 100%;
   margin: 0;
+}
+
+#file-upload-input {
+  display: none;
 }
 </style>
